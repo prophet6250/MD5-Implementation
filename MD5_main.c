b3951832978889fbc7fee545e3947c54b851b3e0
Bad_Boy17 <44083056+prophet6250@users.noreply.github.com>
2020-04-27 10:23:01 +0530

    Update MD5_main.c
    the hash engine accepts all sizes of strings now + some optimizations

--- MD5_main.c
+++ MD5_main.c
@@ -1,40 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
-#define F(x, y, z) ((x & y) | (~(x) & z))
-#define G(x, y, z) ((x & y) | (y & ~(z)))
-#define H(x, y, z) (x ^ y ^ z)
-#define I(x, y, z) (y ^ (x | ~(z)))
-#define ROTL(x, n) ((x << n) | (x >> (32 - n)))
+#define ROTL(x, n) ((x << n) | (x >> (32-n)))
 
 void
-add_padding(unsigned char *text, unsigned long long int size, int zeroes)
+MD5_engine(const unsigned char *text, 
+           const int totat_blocks, 
+           unsigned char *output) 
 {
-	int index = 1, factor = 1;
-	
-	text[size] = 0x01;
-	while (index < zeroes) {
-		text[size + index] = 0x00;
-		index += 1;
-	}
-	
-	index = 0;
-	text[63 - index] = 0xFF & size;
-	index += 1;
-	while (factor < 8) {
-		text[63 - index] = 0xFF & (size >> (8 * factor));
-		index += 1;
-		factor += 1;
-	}
-	return;
-}
+	/* first four initial constants of the digest */
+	unsigned int a0 = 0x67452301, 
+	             b0 = 0xefcdab89, 
+	             c0 = 0x98badcfe, 
+	             d0 = 0x10325476, 
+	             block_no = 0;
+	unsigned int chunks[16];
+	unsigned int i, j;
+	unsigned char block[64] = "\0";
 
-void
-create_MD5_digest(const unsigned char *text, unsigned char *digest)
-{
-
-	/* this is just floor(2^32 * abs(sin(i + 1)), where i is from 0 to 63 */
+	/* this is floor(2^32 * abs(sin(i + 1)), where i is from 0 to 63 */
 	const unsigned int Constants[] = {
 		0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
 		0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
@@ -56,113 +42,129 @@ create_MD5_digest(const unsigned char *text, unsigned char *digest)
 
 	/* number of rotations to perform per 512 bit chunk per round*/
 	const unsigned int rotations[] = {
-		7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,7, 12, 17, 22,
-		5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
-		4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-		6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+		7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+		5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+		4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+		6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21
 	};
 
-	/* first four initial constants of the digest */
-	unsigned int A = 0x67452301,  B = 0xefcdab89, C = 0x98badcfe,
-	             D = 0x10325476, i, j;
-	unsigned int chunks[16];
-	unsigned int AA = A, BB = B, CC = C, DD = D;
+        
+	while (block_no < totat_blocks) {
+		/* copy the 64 byte block, and then move the pointer to the 
+		 * next 64 bytes block */
+		for (i = block_no * 64, j = 0; j < 64; i++, j++) {
+			block[j] = text[i];
+		}
 
-	/* breaking the modified message into 16 4 byte chunks */
-	for (i = 0, j = 0; i < 16 && j < 64; i++, j += 4) {
-		chunks[i] = text[j] | text[j + 1] | text[j + 2] | text[j + 3];
+		/* initialize the words for this iteration */
+		unsigned int A = a0, B = b0, C = c0, D = d0;
+
+	        /* breaking the modified message into 16 4 byte chunks.
+	         * again, big endian ordering has been maintained */
+		for (i = 0, j = 0; i < 16; i++, j += 4) {
+			chunks[i] = ((block[j] << 24) | (block[j + 1] << 16) | 
+				(block[j + 2]  << 8) | (block[j + 3]));
+		}
+
+		/* do the transformations on current hash words */
+		for (i = 0; i < 64; i++) {
+			unsigned int f, g;
+
+			if (i >= 0 && i <= 15) {
+				f = (B & C) | ((~B) & D);
+				g = i;
+			} 
+			if (i >= 16 && i <= 31) {
+				f = (D & B) | ((~D) & C);
+				g = (5 * i + 1) % 16;
+			}
+			if (i >= 32 && i <= 47) {
+				f = B ^ C ^ D;
+				g = (3 * i + 5) % 16;
+			}
+			if (i >= 48 && i <= 63) {
+				f = C ^ (B | (~D));
+				g = (7 * i) % 16;
+			}
+
+			f = f + A + Constants[i] + chunks[g];
+			A = D;
+			D = C;
+			C = B;
+			B = B + ROTL(f, rotations[i]);
+		}
+
+		/* add this chunk's hash so far */
+		a0 += A;
+		b0 += B;
+		c0 += C;
+		d0 += D;
+
+		block_no += 1;
 	}
-	
-	for (i = 0; i < 64; i++) {
-		unsigned int f, g;
 
-		if (i >= 0 && i <= 15) {
-			f = F(B, C, D);
-			g = i;
-		} 
-		else if (i >= 16 && i <= 31) {
-			f = G(B, C, D);
-			g = (5 * i + 1) % 16;
-		}
-		else if (i >= 32 && i <= 47) {
-			f = H(B, C, D);
-			g = (3 * i + 5) % 16;
-		}
-		else {
-			f = I(B, C, D);
-			g = (7 * i) % 16;
-		}
+	/* append all the words into a single hash string */
+	output[0]  = (a0 >> 24) & 0xFF; 
+	output[1]  = (a0 >> 16) & 0xFF;
+	output[2]  = (a0 >>  8) & 0xFF;
+	output[3]  = (a0)       & 0xFF;
 
-		f = f + AA + Constants[i] + chunks[g];
-		AA = DD;
-		DD = CC;
-		CC = BB;
-		BB = BB + ROTL(f, rotations[i]);
-	}
+	output[4]  = (b0 >> 24) & 0xFF;
+	output[5]  = (b0 >> 16) & 0xFF;
+	output[6]  = (b0 >>  8) & 0xFF;
+	output[7]  = (b0)       & 0xFF;
 
-	/* add this chunk's hash so far (in case of multiple chunks) */
-	A += AA;
-	B += BB;
-	C += CC;
-	D += DD;
+	output[8]  = (c0 >> 24) & 0xFF; 
+	output[9]  = (c0 >> 16) & 0xFF;
+	output[10] = (c0 >>  8) & 0xFF;
+	output[11] = (c0)       & 0xFF;
 
-	printf("%x %x %x %x\n", A, B, C, D);
-
-	digest[0]  = (A >> 24) & 0xFF;
-	digest[1]  = (A >> 16) & 0xFF;
-	digest[2]  = (A >> 8) & 0xFF;
-	digest[3]  = A & 0xFF;
-
-	digest[4]  = (B >> 24) & 0xFF;
-	digest[5]  = (B >> 16) & 0xFF;
-	digest[6]  = (B >> 8) & 0xFF;
-	digest[7]  = B & 0xFF;
-	
-	digest[8]  = (C >> 24) & 0xFF;
-	digest[9]  = (C >> 16) & 0xFF;
-	digest[10] = (C >> 8) & 0xFF;
-	digest[11] = C & 0xFF;
-	
-	digest[12] = (D >> 24) & 0xFF;
-	digest[13] = (D >> 16) & 0xFF;
-	digest[14] = (D >> 8) & 0xFF;
-	digest[15] = D & 0xFF;
+	output[12] = (d0 >> 24) & 0xFF;
+	output[13] = (d0 >> 16) & 0xFF;
+	output[14] = (d0 >>  8) & 0xFF;
+	output[15] = (d0)       & 0xFF;
 }
 
 void
-print(const unsigned char *string, int size)
+add_padding(unsigned char *padded, 
+            unsigned long long int size, 
+            const unsigned long long int sizeof_hash)
+{	
+	padded[size] = 0x80;
+	/* append the sizeof original string at the end of the padded string.
+	 * big endian ordering has been maintained */
+	padded[sizeof_hash - 8] = (size >> 56) & 0xFF;
+	padded[sizeof_hash - 7] = (size >> 48) & 0xFF;
+	padded[sizeof_hash - 6] = (size >> 40) & 0xFF;
+	padded[sizeof_hash - 5] = (size >> 32) & 0xFF;
+	padded[sizeof_hash - 4] = (size >> 24) & 0xFF;
+	padded[sizeof_hash - 3] = (size >> 16) & 0xFF;
+	padded[sizeof_hash - 2] = (size >>  8) & 0xFF;
+	padded[sizeof_hash - 1] = (size)       & 0xFF;
+}
+
+int main(int argc, const char *argv[])
 {
-	printf("\n");
-	int index = 0;
-	while (index < size) {
-		printf("%x", string[index]);
+	const unsigned long long int size = strlen(argv[1]);
+	unsigned long long int sizeof_hash = ceil(size / 64.0) * 64;
+	unsigned int block_count, index = 0;
+
+	if (size == 64) sizeof_hash += 64;
+	if (size == 0) sizeof_hash = 64;
+
+	unsigned char *padded = malloc(sizeof *padded * sizeof_hash);
+	unsigned char digest[16];
+
+	block_count = sizeof_hash / 64;	
+
+	strncpy(padded, argv[1], size);
+	add_padding(padded, size, sizeof_hash);
+	MD5_engine(padded, block_count, digest);
+
+	index = 0;
+	while (index < 16) {
+		printf("%x", digest[index]);
 		index += 1;
 	}
 	printf("\n");
-	return;
 }
-
-int main(int argc, char *argv[])
-{
-	/* parse 64 bytes at a time when dealing with strlen(plainetxt) >= 512
-	 */
-	unsigned char *plaintext = "Enter your string here :)";
-	unsigned char digest[16];
-	unsigned char ciphertext[64];
-	unsigned long long int sizeof_plaintext = strlen(plaintext);
-	int zeroes = 56 - sizeof_plaintext - 1;
-
-	strncpy(ciphertext, plaintext, sizeof_plaintext);
-	
-	/* 1), 2) append a 1, some 0s and the sizeof plaintext in the end */
-	add_padding(ciphertext, sizeof_plaintext, zeroes);
-	
-	/* 3) initialize the MD buffer and do the transformations */
-	create_MD5_digest(ciphertext, digest);
-
-	/* 5) Output the hashed message */
-	print(digest, 16);
-	
-	return 0;
-}
-
